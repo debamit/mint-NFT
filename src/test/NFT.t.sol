1ac376b2117e70b31c4cc8127a848cf20834c117
debamit <debamit@outlook.com>
2022-06-02 12:36:40 -0700

    Add more tests.

--- src/test/NFT.t.sol
+++ src/test/NFT.t.sol
@@ -25,4 +25,44 @@ contract NFTTest is DSTest {
     function testMintPricePaid() public {
         nft.mintTo{value: 0.08 ether}(address(1));
     }
+
+    function testFailMaxSupplyReached() public {
+        uint256 slot = stdstore.target(address(nft)).sig("currentTokenId()").find();
+        bytes32 loc = bytes32(slot);
+        bytes32 mockedCurrentTokenId = bytes32(abi.encode(10000));
+        vm.store(address(nft), loc, mockedCurrentTokenId);
+        nft.mintTo{value: 0.08 ether}(address(1));
+    }
+
+    function testFailMintToZeroAddress() public {
+        nft.mintTo{value: 0.08 ether}(address(0));
+    }
+
+    function testNewMintOwnerRegistered() public {
+        nft.mintTo{value: 0.08 ether}(address(1));
+        uint256 slotOfNewOwner = stdstore
+            .target(address(nft))
+            .sig(nft.ownerOf.selector)
+            .with_key(1)
+            .find();
+
+        uint160 ownerOfTokenIdOne = uint160(uint256((vm.load(address(nft),bytes32(abi.encode(slotOfNewOwner))))));
+        assertEq(address(ownerOfTokenIdOne), address(1));
+    }
+
+    function testBalanceIncremented() public { 
+        nft.mintTo{value: 0.08 ether}(address(1));
+        uint256 slotBalance = stdstore
+            .target(address(nft))
+            .sig(nft.balanceOf.selector)
+            .with_key(address(1))
+            .find();
+        
+        uint256 balanceFirstMint = uint256(vm.load(address(nft), bytes32(slotBalance)));
+        assertEq(balanceFirstMint, 1);
+
+        nft.mintTo{value: 0.08 ether}(address(1));
+        uint256 balanceSecondMint = uint256(vm.load(address(nft), bytes32(slotBalance)));
+        assertEq(balanceSecondMint, 2);
+    }
 }
